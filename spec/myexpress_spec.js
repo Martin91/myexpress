d2432bad7a5610d948b08630b2de84d1c4711090
Martin <hongzeqin@gmail.com>
2014-04-02 21:40:43 +0800

    modify the test file

--- spec/myexpress_spec.js
+++ spec/myexpress_spec.js
@@ -4,26 +4,30 @@ var express   = require("../")
     , request = require("supertest")
     , express = require("myexpress");
 
-var app       = express()
-    , server  = http.createServer(app);
-
-server.listen(4000);
-
 describe('app', function(){
+  var app = express();
+
   describe("create http server", function() {
     it("should render 404 for empty app", function(done) {
+      var server = http.createServer(app);
       request(server).get('/').expect(404, done);
     });
   });
 
   describe('create listen method', function() {
-    server = app.listen(4000);
+    var port = 8888;
+    var server;
+
+    before(function(done) {
+      server = app.listen(port, done);
+    });
+
     it("should return http.Server", function() {
       expect(server).to.be.instanceof(http.Server);
     });
 
     it("should render 404", function(done) {
-      request(server).get('/').expect(404, done);
+      request('http://localhost:'+port).get('/something-not-found').expect(404, done);
     });
   });
 });
