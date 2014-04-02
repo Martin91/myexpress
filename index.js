f5843a98deba054c9ed8d9b542b3f7ea0d769d97
Martin <hongzeqin@gmail.com>
2014-04-02 16:35:40 +0800

    add listen method

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+var http = require("http");
+
 module.exports = function() {
   // Act as requestListener, a function which is automatically
   // added to the 'request' event. http://nodejs.org/api/http.html#http_event_request
-  return function(request, response) {
+  var myexpress = function(request, response) {
     response.statusCode = 404;
     response.end();
   };
+
+  myexpress.listen = function(port) {
+    var server = http.createServer(this);
+    server.listen(port);
+
+    return server;
+  }
+
+  return myexpress;
 };
--- spec/myexpress_spec.js
+++ spec/myexpress_spec.js
@@ -1,4 +1,5 @@
 var express   = require("../")
+    , expect  = require("chai").expect
     , http    = require("http")
     , request = require("supertest")
     , express = require("myexpress");
@@ -10,7 +11,18 @@ server.listen(4000);
 
 describe('app', function(){
   describe("create http server", function() {
-    it("should render 404 for empty app", function(done){
+    it("should render 404 for empty app", function(done) {
+      request(server).get('/').expect(404, done);
+    });
+  });
+
+  describe('create listen method', function() {
+    server = app.listen(4000);
+    it("should return http.Server", function() {
+      expect(server).to.be.instanceof(http.Server);
+    });
+
+    it("should render 404", function(done) {
       request(server).get('/').expect(404, done);
     });
   });
