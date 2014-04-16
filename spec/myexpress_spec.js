ec5870a88442fc21ba68282826afd8b691377f52
Martin <hongzeqin@gmail.com>
2014-04-16 21:35:53 +0800

    test for unhandled error

--- spec/myexpress_spec.js
+++ spec/myexpress_spec.js
@@ -93,3 +93,19 @@ describe('calling middleware stack', function() {
     request(app).get('/').expect(404, done);
   });
 });
+
+describe('error handling', function(){
+  var app;
+  beforeEach(function() {
+    app = new express();
+  });
+
+  it('should return 500 for unhandled error', function(done){
+    var m1 = function(req,res,next) {
+      next(new Error("boom!"));
+    }
+    app.use(m1);
+
+    request(app).get('/').expect(500, done);
+  });
+});
